107cb962d9a47410a2e59ad4d1086161f7af59b6
eghc <eringraceharris@gmail.com>
2019-02-03 13:26:25 -0800

    Basic Node App complete

---
+++ main.js
@@ -0,0 +1,16 @@
+const greetings = require("./greeting.js");
+const otherGreetings = require("./otherGreetings.js");
+
+// "Hello"
+greetings.greetingEnglish();
+
+// "Konichiwa"
+greetings.greetingJapanese();
+
+greetings.greetingSpanish();
+
+greetings.greetingIcelandic();
+
+otherGreetings.greetingSpanishCasual();
+
+otherGreetings.greetingEnglishCasual();
---
+++ otherGreetings.js
@@ -0,0 +1,7 @@
+exports.greetingEnglishCasual = function(){
+  console.log("What's up?");
+}
+
+exports.greetingSpanishCasual = function(){
+  console.log("¿Que pasa?");
+}
